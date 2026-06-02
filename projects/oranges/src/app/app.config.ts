786c8ae96a577feb63500c39b248a1293f01fec0
Sam Severance <swseverance@gmail.com>
2026-06-02 19:03:02 -0400

    ok

--- projects/oranges/src/app/app.config.ts
+++ projects/oranges/src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
